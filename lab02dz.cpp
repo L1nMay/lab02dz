56a49262cabbf0467fa9b9ca254e3f0070ac8ef1
Lin <ilazayzev@gmail.com>
2025-02-22 22:08:57 +0300

    I am add comments to the code

--- lab02dz.cpp
+++ lab02dz.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <string>
+#include <iostream> // For input and output
+#include <string> // For string manipulation
 
 int main() {
-  std::string name;
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  std::cout << "Hello world from " << name << std::endl;
-  return 0;
+  std::string name; // Variable to store the user's name
+  std::cout << "Enter your name: "; // Prompt the user for their name
+  std::cin >> name; // Read the user's name from standard input
+  std::cout << "Hello world from " << name << std::endl; // Print a personalized greeting
+  return 0; // Indicate successful execution
 }
